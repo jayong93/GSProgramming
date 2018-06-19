2de061b0e5a6e64cc4cb3c6539398da5878c9663
Choi Jae-Yong <jayong93@gmail.com>
2018-06-19 19:07:23 +0900

    Attacked 메세지 추가

--- Share/Shares.h
+++ Share/Shares.h
@@ -2,8 +2,8 @@
 #include "MsgReconstructor.h"
 #include <cassert>
 
-enum class MsgTypeCS { NONE, CS_MOVE_UP, CS_MOVE_DOWN, CS_MOVE_LEFT, CS_MOVE_RIGHT, CS_CHAT, CS_TELEPORT };
-enum class MsgTypeSC { NONE, SC_MOVE_OBJ, SC_PUT_OBJ, SC_REMOVE_OBJ, SC_CHAT, SC_GIVE_ID, SC_DETAIL_DATA, SC_SET_HP, SC_SET_MAX_HP, SC_CHANGE_LEVEL };
+enum class MsgTypeCS { NONE, CS_MOVE_UP, CS_MOVE_DOWN, CS_MOVE_LEFT, CS_MOVE_RIGHT, CS_CHAT, CS_TELEPORT, CS_ATTACK };
+enum class MsgTypeSC { NONE, SC_MOVE_OBJ, SC_PUT_OBJ, SC_REMOVE_OBJ, SC_CHAT, SC_GIVE_ID, SC_DETAIL_DATA, SC_SET_HP, SC_SET_MAX_HP, SC_CHANGE_LEVEL, SC_ATTACKED };
 enum class ConnectionType { NORMAL, HOTSPOT };
 enum class ObjectType { OBJECT, PLAYER, MELEE, RANGE };
 
@@ -36,31 +36,31 @@ struct MsgBase {
 };
 
 struct MsgInputMove : public MsgBase {
-	MsgInputMove(MsgTypeCS type) : MsgBase{ sizeof(MsgInputMove), type } {}
+	MsgInputMove(MsgTypeCS type) : MsgBase{ sizeof(decltype(*this)), type } {}
 };
 
 struct MsgGiveID : public MsgBase {
 	unsigned int id;
 
-	explicit MsgGiveID(unsigned int id) : MsgBase{ sizeof(MsgGiveID), MsgTypeSC::SC_GIVE_ID }, id{ id } {}
+	explicit MsgGiveID(unsigned int id) : MsgBase{ sizeof(decltype(*this)), MsgTypeSC::SC_GIVE_ID }, id{ id } {}
 };
 
 struct MsgMoveObject : public MsgBase {
 	WORD id;
 	WORD x, y;
-	MsgMoveObject(unsigned int id, short x, short y) : MsgBase{ sizeof(MsgMoveObject), MsgTypeSC::SC_MOVE_OBJ }, id( id ), x( x ), y( y ) {}
+	MsgMoveObject(unsigned int id, short x, short y) : MsgBase{ sizeof(decltype(*this)), MsgTypeSC::SC_MOVE_OBJ }, id( id ), x( x ), y( y ) {}
 };
 
 struct MsgRemoveObject : public MsgBase {
 	WORD id;
-	explicit MsgRemoveObject(unsigned int id) : MsgBase{ sizeof(MsgRemoveObject), MsgTypeSC::SC_REMOVE_OBJ }, id( id ) {}
+	explicit MsgRemoveObject(unsigned int id) : MsgBase{ sizeof(decltype(*this)), MsgTypeSC::SC_REMOVE_OBJ }, id( id ) {}
 };
 
 struct MsgPutObject : public MsgBase {
 	WORD id;
 	WORD x, y;
 
-	MsgPutObject(unsigned int id, short x, short y) : MsgBase{ sizeof(MsgPutObject), MsgTypeSC::SC_PUT_OBJ }, id( id ), x( x ), y( y ) {}
+	MsgPutObject(unsigned int id, short x, short y) : MsgBase{ sizeof(decltype(*this)), MsgTypeSC::SC_PUT_OBJ }, id( id ), x( x ), y( y ) {}
 };
 
 struct MsgDetailData : public MsgBase {
@@ -68,20 +68,20 @@ struct MsgDetailData : public MsgBase {
 	Color color;
 	ObjectType objType;
 
-	MsgDetailData(unsigned int id, Color color, ObjectType type) : MsgBase{ sizeof(MsgDetailData), MsgTypeSC::SC_DETAIL_DATA }, id{ id }, color { color }, objType{ type } {}
+	MsgDetailData(unsigned int id, Color color, ObjectType type) : MsgBase{ sizeof(decltype(*this)), MsgTypeSC::SC_DETAIL_DATA }, id{ id }, color { color }, objType{ type } {}
 };
 
 struct MsgTeleport : public MsgBase {
 	short x, y;
 
-	MsgTeleport(short x, short y) : MsgBase{ sizeof(MsgTeleport), MsgTypeCS::CS_TELEPORT }, x{ x }, y{ y } {}
+	MsgTeleport(short x, short y) : MsgBase{ sizeof(decltype(*this)), MsgTypeCS::CS_TELEPORT }, x{ x }, y{ y } {}
 };
 
 struct MsgChat : public MsgBase {
 	WORD from;
 	wchar_t msg[MAX_CHAT_LEN];
 
-	MsgChat(unsigned int from, const wchar_t* msg) : MsgBase{ sizeof(MsgChat), MsgTypeSC::SC_CHAT }, from( from ) {
+	MsgChat(unsigned int from, const wchar_t* msg) : MsgBase{ sizeof(decltype(*this)), MsgTypeSC::SC_CHAT }, from( from ) {
 		lstrcpyn(this->msg, msg, MAX_CHAT_LEN);
 	}
 };
@@ -90,20 +90,28 @@ struct MsgSetHP : public MsgBase {
 	unsigned int id;
 	int hp;
 
-	MsgSetHP(unsigned int id, int hp) : MsgBase{ sizeof(MsgSetHP), MsgTypeSC::SC_SET_HP }, id{ id }, hp{ hp } {}
+	MsgSetHP(unsigned int id, int hp) : MsgBase{ sizeof(decltype(*this)), MsgTypeSC::SC_SET_HP }, id{ id }, hp{ hp } {}
 };
 
 struct MsgSetMaxHP : public MsgBase {
 	unsigned int id;
 	int maxHP;
 
-	MsgSetMaxHP(unsigned int id, int maxHP) : MsgBase{ sizeof(MsgSetMaxHP), MsgTypeSC::SC_SET_MAX_HP }, id{ id }, maxHP{ maxHP } {}
+	MsgSetMaxHP(unsigned int id, int maxHP) : MsgBase{ sizeof(decltype(*this)), MsgTypeSC::SC_SET_MAX_HP }, id{ id }, maxHP{ maxHP } {}
 };
 
 struct MsgChangeLevel : public MsgBase {
 	unsigned int id;
 	int level;
 
-	MsgChangeLevel(unsigned int id, int level) : MsgBase{ sizeof(MsgChangeLevel), MsgTypeSC::SC_CHANGE_LEVEL }, id{ id }, level{ level } {}
+	MsgChangeLevel(unsigned int id, int level) : MsgBase{ sizeof(decltype(*this)), MsgTypeSC::SC_CHANGE_LEVEL }, id{ id }, level{ level } {}
+};
+
+struct MsgAttacked : public MsgBase {
+	unsigned int id;
+	unsigned int from;
+	int damage;
+
+	MsgAttacked(unsigned int id, unsigned int from, int damage) : MsgBase{ sizeof(decltype(*this)), MsgTypeSC::SC_CHANGE_LEVEL }, id{ id }, from{ from }, damage{ damage } {}
 };
 #pragma pack(pop)
